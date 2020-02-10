8560410d7437b89be2e7d06e2a2ca011e61cac25
chenc <double_cl@163.com>
2020-02-10 21:35:50 +0800

    feat: 接口转发

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { tap } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 
 @Injectable({
@@ -10,13 +9,8 @@ export class LoginService {
   constructor(readonly http: HttpClient) {}
 
   submitLogin(data: any) {
-    console.log('---submitLogin---');
     return this.http
-      .get<{ name: string }>(environment.apiUrl + '/api/v1/demo')
-      .pipe(
-        tap((newHero: { name: string }) =>
-          console.log(`added hero w/ id=${newHero}`)
-        )
-      );
+      .post<{ name: string }>(environment.apiUrl + '/api/v1/login', data)
+      .subscribe(x => console.log('x', x));
   }
 }
