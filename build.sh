cd5d5890b917ab8ebe8151e77abba32a9e2767c2
chenc <double_cl@163.com>
2022-01-30 20:57:21 +0800

    chore: modify desc info (#48)

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ echo 'build redis app end'
 
 echo 'start build docker ----> '
 
-docker build -t registry.cn-shanghai.aliyuncs.com/chenc/redis-app:2.0.1 .
-docker push registry.cn-shanghai.aliyuncs.com/chenc/redis-app:2.0.1
+docker build -t registry.cn-shanghai.aliyuncs.com/chenc/redis-app:2.0.2 .
+docker push registry.cn-shanghai.aliyuncs.com/chenc/redis-app:2.0.2
 
 echo 'build docker end'
