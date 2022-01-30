a28a2204c969ff69b68b7fc1b6f271f1dd35eed7
chenc <double_cl@163.com>
2022-01-30 20:41:54 +0800

    chore: build redis app image  and push image hub

--- build.sh
+++ build.sh
@@ -1,2 +1,12 @@
-docker build -t registry.cn-shanghai.aliyuncs.com/chenc/redis-app:1.1.3 .
-docker push registry.cn-shanghai.aliyuncs.com/chenc/redis-app:1.1.3
+echo 'start build redis app'
+
+pnpm run build
+
+echo 'build redis app end'
+
+echo 'start build docker ----> '
+
+docker build -t registry.cn-shanghai.aliyuncs.com/chenc/redis-app:2.0.0 .
+docker push registry.cn-shanghai.aliyuncs.com/chenc/redis-app:2.0.0
+
+echo 'build docker end'
