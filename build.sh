60bde086ecef7b810665da1c800b5a2b5cd6bf68
chenc <double_cl@163.com>
2022-01-30 20:44:35 +0800

    chore: modify base api url

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ echo 'build redis app end'
 
 echo 'start build docker ----> '
 
-docker build -t registry.cn-shanghai.aliyuncs.com/chenc/redis-app:2.0.0 .
-docker push registry.cn-shanghai.aliyuncs.com/chenc/redis-app:2.0.0
+docker build -t registry.cn-shanghai.aliyuncs.com/chenc/redis-app:2.0.1 .
+docker push registry.cn-shanghai.aliyuncs.com/chenc/redis-app:2.0.1
 
 echo 'build docker end'
