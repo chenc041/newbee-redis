ab25d2723b5ac4806444d8b86bfb53474385adc6
chenc <double_cl@163.com>
2022-01-30 10:03:02 +0800

    feature(dashboard): modify the db select component

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -9,11 +9,11 @@ import { NzModalModule } from 'ng-zorro-antd/modal';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
-import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { NzSelectModule } from 'ng-zorro-antd/select';
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
@@ -28,9 +28,9 @@ import { SharedModule } from 'src/app/shared/shared.module';
     NzDividerModule,
     NzLayoutModule,
     NzToolTipModule,
-    NzDropDownModule,
     NzPopconfirmModule,
     SharedModule,
+    NzSelectModule,
   ],
 })
 export class DashboardModule {}
